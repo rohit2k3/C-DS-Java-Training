b68470c4d66515dcb65d4a6e44ea39c8b4ec5813
rohit2k3 <rohitsharma2k3@gmail.com>
2024-07-19 16:20:12 +0530

    Added Programs

---
+++ LengthOfname.java
@@ -0,0 +1,12 @@
+package com.rohit.Learnovate;
+
+public class LengthOfname {
+    public static void main(String[] args) {
+        String name = "Rohit Sharma";
+        int count = 0;
+        for (char a: name.toCharArray()){
+            count++;
+        }
+        System.out.println(count);
+    }
+}
---
+++ SwapProgram.java
@@ -0,0 +1,30 @@
+package com.rohit.Learnovate;
+
+public class SwapProgram {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int temp;
+        swapWithoutThird(a,b);
+        SwapWithThird(a ,b);
+    }
+
+    public static void SwapWithThird(int a , int b){
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        // Swapping logic using a third variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+
+    public static void swapWithoutThird(int a , int b){
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+        a = a+b;
+        b  = a-b;
+        a = a-b;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
