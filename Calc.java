87fa895033edf2542ad9d395c870611d9390f962
rohit2k3 <rohitsharma2k3@gmail.com>
2024-07-22 15:19:51 +0530

    day 2

--- Calc.java
+++ Calc.java
@@ -4,8 +4,7 @@ public class Calc {
     public static void main(String[] args) {
         int a = 5;
         float b = 7.5F;
-        float sum = a+b;
-        System.out.println(sum);
+        System.out.println("sum is "+ (a+b));
     }
 
 }
