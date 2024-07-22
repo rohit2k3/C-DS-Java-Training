86d03456e191eb02741b3d3f610c6ff992cbe833
rohit2k3 <rohitsharma2k3@gmail.com>
2024-07-22 15:39:53 +0530

    day 3

---
+++ VoteAgeCheck.java
@@ -0,0 +1,12 @@
+package com.rohit.Learnovate;
+
+public class VoteAgeCheck {
+    public static void main(String[] args) {
+        int age = 31;
+        if (age > 18){
+            System.out.println("You are eligible for voting");
+        }else {
+            System.out.println("You are not eligible for voting");
+        }
+    }
+}
