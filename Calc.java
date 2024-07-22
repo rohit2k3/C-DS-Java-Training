6721235750696bc46924de585d2f6ed7a7dd6fbb
rohit2k3 <rohitsharma2k3@gmail.com>
2024-07-22 15:04:53 +0530

    day 2

---
+++ Calc.java
@@ -0,0 +1,11 @@
+package com.rohit.Learnovate;
+
+public class Calc {
+    public static void main(String[] args) {
+        int a = 5;
+        float b = 7.5F;
+        float sum = a+b;
+        System.out.println(sum);
+    }
+
+}
