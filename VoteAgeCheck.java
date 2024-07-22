7fad89a4aec6548028241d99f0263d9f645107f5
rohit2k3 <rohitsharma2k3@gmail.com>
2024-07-22 15:46:41 +0530

    added new method (day3)

--- VoteAgeCheck.java
+++ VoteAgeCheck.java
@@ -3,6 +3,12 @@ package com.rohit.Learnovate;
 public class VoteAgeCheck {
     public static void main(String[] args) {
         int age = 18;
+
+        //without if else
+        String message = (age >= 18) ? "You are eligible for voting" : "You are not eligible for voting";
+        System.out.println(message);
+
+
         if (age >= 18){
             System.out.println("You are eligible for voting");
         }else {
