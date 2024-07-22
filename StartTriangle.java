28e27d57a4204b00acfb19f9114a2c9d67714e7f
rohit2k3 <rohitsharma2k3@gmail.com>
2024-07-22 16:16:57 +0530

    day 3 (added new method)

--- StartTriangle.java
+++ StartTriangle.java
@@ -4,7 +4,7 @@ public class StartTriangle {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(" *");
             }
             System.out.println();
         }
