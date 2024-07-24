9b824b6af87032e05f7bea3adae1f08960de36c1
rohit2k3 <rohitsharma2k3@gmail.com>
2024-07-24 14:33:50 +0530

    day 5

---
+++ BreakPractice.java
@@ -0,0 +1,14 @@
+package com.rohit.Learnovate;
+
+public class BreakPractice {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i==6){
+//                break;
+                continue;
+            }
+            System.out.println(i);
+
+        }
+    }
+}
